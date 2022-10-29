e01d5e9379d40b41f920f82b1e4d0138aff669c8
unknown <willpattisons@yahoo.com>
2022-10-28 20:57:49 -0500

    added responsive viewItem component

--- client/src/app/view-item/view-item.component.ts
+++ client/src/app/view-item/view-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-view-item',
@@ -7,7 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewItemComponent implements OnInit {
 
+  @ViewChild('slickModal')slickModal:ElementRef;
+
+  slideConfig = {
+    "slidesToShow": 3,
+    "slidesToScroll": 1,
+    "dots": false,
+    "infinite": true,
+    "autoplay" : true,
+    "autoplaySpeed" : 3000,
+    responsive: [
+      {
+        breakpoint: 2800,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings:{
+          slidesToShow:1,
+          slidesToScroll:1
+        }
+      }
+    ]
+  };
+
+  slideCounter:number = 0;
+  totalSlides:number = 4;
+
   constructor() { }
+
+
+  onHandleSlickCounter(modal){
+    modal.slickGoTo((this.slideCounter+1));
+    this.slideCounter = (this.slideCounter+1)%this.totalSlides;
+  }
+
 
   ngOnInit(): void {
   }
